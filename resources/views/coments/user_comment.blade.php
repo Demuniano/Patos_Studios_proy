67240bd30d36f5c629f0be8db2b3cbe51d6fa82d
Demuniano <jrojasroj@unal.edu.co>
2023-11-05 19:53:44 -0500

    user comments view

---
+++ resources/views/coments/user_comment.blade.php
@@ -0,0 +1,21 @@
+@extends('layout.template')
+@section('content')
+<body>
+<h1>Comentarios</h1>
+    <div class="container text-center">
+    <div>
+        <h1>Crear comentario</h1>
+        <form action="{{route('comentsUser.store')}}" method="post">
+            @csrf
+            <label for="descriptionComent">Descripcion: </label>
+            <input type="text" name="descriptionComent" id="description" class="form-control mn-3" required >
+            <label for="qualificationComent">Calificacion: </label>
+            <input type="text" id="qualification"class="form-control mn-3" name="qualificationComent">
+            <button type="submit" class="btn btn-success mt-4">Crear</button>
+        </form>
+        </div>
+    </div>
+
+
+</body>
+@endsection
