c72baff16b4e7345942eaa14a5674be60c42f3dc
Fredy Aguirre <107375460+FredyAguirre@users.noreply.github.com>
2023-11-13 00:20:21 -0500

    se hacen modificaciones visuales al index

--- resources/views/home/home.blade.php
+++ resources/views/home/home.blade.php
@@ -1,8 +1,8 @@
 @extends('layout.template')
 @section('content')
 <html lang="en">
-  <div class="container">
-    <div id="carouselExampleIndicators" class="carousel slide" data-bs-ride="carousel">
+  <div class="container-fluid">
+    <div id="carouselExampleIndicators" class="carousel slide" data-bs-ride="carousel" data-bs-interval="3000">
       <div class="carousel-indicators">
         <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
         <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="1" aria-label="Slide 2"></button>
